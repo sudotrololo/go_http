32826ba215be183b8adf593fff7565e07268ee3d
Algan <algan4@mail.ru>
2023-06-25 14:28:26 +0300

    logger function has been created

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
-	"io"
 )
 
 func main() {
@@ -18,19 +18,21 @@ func main() {
 	// Запускаем сервер на порту 9999
 	port := 9999
 	addr := fmt.Sprintf(":%d", port)
-	log.Printf("Server has been started on port %d", port)
+	resume := fmt.Sprintf("Server has been started on port %d", port)
+	logger(resume)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func getHTMLHandler(w http.ResponseWriter, r *http.Request) {
 	// Открываем файл index.html
 	file, err := http.Dir(".").Open("index.html")
+	logger("open page /get_html", err)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
-
+	
 	// Копируем содержимое файла в ResponseWriter
 	_, err = io.Copy(w, file)
 	if err != nil {
@@ -40,13 +42,14 @@ func getHTMLHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
+	logger("open page /say_hello")
 	fmt.Fprint(w, "Hello!")
 }
 
 func sayCurrentTimeHandler(w http.ResponseWriter, r *http.Request) {
+	logger("open page /say_current_time")
 	// Получаем текущее время по МСК
 	currentTime := time.Now().UTC().Add(time.Hour * 3)
-
 	// Отправляем время в формате строки
 	fmt.Fprint(w, currentTime.Format("15:04:05"))
 }
@@ -69,3 +72,10 @@ func getInfoHandler (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 } 
+func logger(s string, errArray ...error){
+	if (len(errArray) > 0) && (errArray[0]!=nil){ //TODO обрабатывать остальные ошибки кроме 0
+		log.Fatal("Pisec")
+	}
+	
+	log.Printf(s)
+}
